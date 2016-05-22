8404a3b371c63ab524ab24c1fc6c48f5cf4c669e
Dan Wildman <Dan Wildman>
2016-05-22 15:11:04 +0100

    Fixed SmoothScroll

--- js/app.js
+++ js/app.js
@@ -88,7 +88,7 @@ $(function() {
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
       if (target.length) {
         $('html, body').animate({
-          scrollTop: target.offset().top
+          scrollTop: target.offset().top - 150
         }, 1000);
         return false;
       }
